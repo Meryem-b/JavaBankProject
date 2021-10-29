d8d7c96401b238c94509e333b0b895805268c377
meryem sema bolat <76153984+meryem-s@users.noreply.github.com>
2021-10-29 17:34:01 +0300

    Update BanaGoreOop.java

--- src/AtmBank/BanaGoreOop.java
+++ src/AtmBank/BanaGoreOop.java
@@ -5,7 +5,7 @@ package AtmBank;
 
 public class BanaGoreOop {
 
-	    public static void main(String[] args) {
+	    public static void main(String[] args){
 	        
 	        Atm atm = new Atm();
 	        Hesap hesap = new Hesap("1");
